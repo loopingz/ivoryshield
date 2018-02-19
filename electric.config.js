ffedf9ce5e612bd385a811a5e2d9054e7d610953
wilfried <w.martinache@gmail.com>
2018-02-19 22:38:58 +0100

    LZ-20: adapting general layout, applying style

--- electric.config.js
+++ electric.config.js
@@ -7,5 +7,17 @@ module.exports = {
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
 	},
+	envOptions: {
+		website: {
+			basePath: '/'
+		},
+		ghpages: {
+      basePath: '/ivoryshield',
+      deployOptions: {
+        branch: 'gh-pages',
+        repo: 'https://' + process.env['GH_TOKEN'] + '@github.com/loopingz/ivoryshield.git'
+      }
+		}
+	},
 	vendorSrc: ['node_modules/marble/build/fonts/**']
 };
