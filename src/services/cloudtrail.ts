98dfe485d010e28a56754fe85af9de00617454f9
Remi Cattiau <remi@cattiau.com>
2019-07-24 15:05:14 -0700

    Update to Webda 0.11

--- src/services/cloudtrail.ts
+++ src/services/cloudtrail.ts
@@ -4,28 +4,18 @@ import {
   Webda,
   Service,
   AWS
-} from '../services/aws-mixin';
-import {
-  SQSQueue
-} from 'webda';
-import {
-  S3
-} from 'aws-sdk';
-import {
-  ValidatorService
-} from './validator';
-import {
-  CloudTrailSetup
-} from '../configurers/cloudtrail';
-import * as fs from 'fs';
-import * as moment from 'moment';
-import * as zlib from 'zlib';
-import * as elasticsearch from 'elasticsearch';
-import * as PromiseUtil from 'bluebird';
-
+} from "../services/aws-mixin";
+import { SQSQueue } from "webda-aws";
+import { S3 } from "aws-sdk";
+import { ValidatorService } from "./validator";
+import { CloudTrailSetup } from "../configurers/cloudtrail";
+import * as fs from "fs";
+import * as moment from "moment";
+import * as zlib from "zlib";
+import * as elasticsearch from "elasticsearch";
+import * as PromiseUtil from "bluebird";
 
 export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
-
   _events: number;
   _counter: number;
   _s3: S3;
@@ -34,18 +24,22 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
   _elkSetup: Service;
   _validatorService: ValidatorService;
 
-  async init(): Promise < void > {
+  async init(): Promise<void> {
     await super.init();
-    this._validatorService = < ValidatorService > this.getService('IvoryShield/ValidatorService');
-    this._cloudtrailSetup = < CloudTrailSetup > this.getService('IvoryShield/CloudTrailSetup');
+    this._validatorService = <ValidatorService>(
+      this.getService("IvoryShield/ValidatorService")
+    );
+    this._cloudtrailSetup = <CloudTrailSetup>(
+      this.getService("IvoryShield/CloudTrailSetup")
+    );
     if (!this._cloudtrailSetup) {
       return;
     }
     //
     this._params.queue = this._cloudtrailSetup.getQueueUrl();
-    this._elkSetup = < Service > this.getService('IvoryShield/ELKSetup');
+    this._elkSetup = <Service>this.getService("IvoryShield/ELKSetup");
     this._aws = this._getAWS();
-    this._s3 = new(this._getAWS()).S3();
+    this._s3 = new (this._getAWS()).S3();
   }
 
   enable() {
@@ -58,24 +52,21 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
 
     this.callback = this.processTrailQueue.bind(this);
     this._workerReceiveMessage();
-    return new Promise(() => {
-
-    });
+    return new Promise(() => {});
   }
 
   async install(params: any) {
     // Setup Kibana
-
     // Setup all cloudtrails
-
     // Setup S3 bucket
-
     // Setup KMS keys
   }
 
   test(evtFile) {
-    this.log('INFO', 'Testing event from', evtFile);
-    return this.processTrailEvent(JSON.parse(fs.readFileSync(evtFile).toString()));
+    this.log("INFO", "Testing event from", evtFile);
+    return this.processTrailEvent(
+      JSON.parse(fs.readFileSync(evtFile).toString())
+    );
   }
 
   _getAWSForEvent(evt) {
@@ -90,35 +81,40 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
       try {
         await this.saveEvent(evt);
       } catch (err) {
-        this.log('WARN', 'Could not indexed', evt.eventID, evt.eventName);
+        this.log("WARN", "Could not indexed", evt.eventID, evt.eventName);
       }
     }
     let aws = await this._getAWSForEvent(evt);
     try {
-      return this._validatorService.handleEvent(aws, evt, evt.recipientAccountId);
+      return this._validatorService.handleEvent(
+        aws,
+        evt,
+        evt.recipientAccountId
+      );
     } catch (err) {
-      if (err.code && err.code.indexOf('NotFound') >= 0) {
+      if (err.code && err.code.indexOf("NotFound") >= 0) {
         // The resource does not exist anymore
-        this.log('DEBUG', 'Resource vanished', evt.eventID);
+        this.log("DEBUG", "Resource vanished", evt.eventID);
       } else {
-        this.log('ERROR', 'Event error', evt.eventID, err.message);
+        this.log("ERROR", "Event error", evt.eventID, err.message);
       }
     }
   }
 
   async processTrailLog(bucket, key) {
-    this.log('DEBUG', 'Processing log', bucket, key);
-    let s3obj = await this._s3.getObject({
-      Bucket: bucket,
-      Key: key
-    }).promise();
+    let s3obj = await this._s3
+      .getObject({
+        Bucket: bucket,
+        Key: key
+      })
+      .promise();
     let promises = [];
     // @ts-ignore
     let cloudEvents = JSON.parse(zlib.gunzipSync(s3obj.Body)).Records;
     await PromiseUtil.map(cloudEvents, this.processTrailEvent.bind(this), {
       concurrency: 10
     });
-    this.emit('ProcessedEvents', cloudEvents.length);
+    this.emit("ProcessedEvents", cloudEvents.length);
   }
 
   run() {
@@ -126,22 +122,26 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
   }
 
   processTrailQueue(s3evt) {
-    if (s3evt.Event === 's3:TestEvent') {
+    if (s3evt.Event === "s3:TestEvent") {
       return;
     }
-    var rex = new RegExp("nuxeo-.*/AWSLogs/\\d+/CloudTrail/.*/\\d{4}/\\d{2}/\\d{2}/\\d+_CloudTrail_.*_(\\d{8})T(\\d{4})Z_.*\\.json\\.gz");
+    var rex = new RegExp(
+      "nuxeo-.*/AWSLogs/\\d+/CloudTrail/.*/\\d{4}/\\d{2}/\\d{2}/\\d+_CloudTrail_.*_(\\d{8})T(\\d{4})Z_.*\\.json\\.gz"
+    );
     if (!s3evt.Records) {
-      throw new Error('Unkown S3 event');
+      throw new Error("Unkown S3 event");
     }
     let promises = [];
-    s3evt.Records.forEach((evt) => {
-      if (evt.eventName === 'ObjectCreated:Put') {
+    s3evt.Records.forEach(evt => {
+      if (evt.eventName === "ObjectCreated:Put") {
         let res = rex.exec(evt.s3.object.key);
         if (!res) {
           return;
         }
         // Only check for cloudtrail now
-        promises.push(this.processTrailLog(evt.s3.bucket.name, evt.s3.object.key));
+        promises.push(
+          this.processTrailLog(evt.s3.bucket.name, evt.s3.object.key)
+        );
       }
     });
     return Promise.all(promises);
@@ -152,7 +152,8 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
       this._es = new elasticsearch.Client({
         host: this._params.elasticsearch
       });
-      this._params.elasticsearchIndex = this._params.elasticsearchIndex || 'logstash-';
+      this._params.elasticsearchIndex =
+        this._params.elasticsearchIndex || "logstash-";
     }
   }
 
@@ -161,17 +162,16 @@ export default class CloudTrailService extends AWSServiceMixIn(SQSQueue) {
       await this.initES();
     }
     let esData: any = {};
-    esData.index = this._params.elasticsearchIndex + evt.eventTime.substring(0, 10).replace(new RegExp('-', 'g'), '.');
+    esData.index =
+      this._params.elasticsearchIndex +
+      evt.eventTime.substring(0, 10).replace(new RegExp("-", "g"), ".");
     esData.id = evt.eventID;
-    esData.type = 'cloudtrail';
+    esData.type = "cloudtrail";
     evt.eventSubtype = evt.eventName.match(/[A-Z][a-z]+/g)[0];
     evt.accountName = this.getAccountName(evt.recipientAccountId);
     esData.body = evt;
     return this._es.create(esData);
   }
-
 }
 
-export {
-  CloudTrailService
-}
+export { CloudTrailService };
